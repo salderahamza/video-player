71a5d456775daa235f1ef3c6095a767aeee72357
dev-zarl <hamzasafdark@gmail.com>
2020-09-18 23:06:26 +0500

    set video progress

--- script.js
+++ script.js
@@ -42,6 +42,12 @@ function updateProgress() {
   currentTime.textContent = `${displayTime(video.currentTime)} /`;
   duration.textContent = `${displayTime(video.duration)}`;
 }
+
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -53,3 +59,4 @@ playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
