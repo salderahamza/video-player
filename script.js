75e5439c8a1546b9c6d53bff1da26f10297f61e7
dev-zarl <hamzasafdark@gmail.com>
2020-09-19 00:03:48 +0500

    mute issue resolved

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   const progressRange = document.querySelector(".progress-range");
   const progressBar = document.querySelector(".progress-bar");
   const playBtn = document.getElementById("play-btn");
-  const volumeIcon = document.getElementById(".volume-icon");
+  const volumeIcon = document.querySelector(".volume-icon");
   const volumeRange = document.querySelector(".volume-range");
   const volumeBar = document.querySelector(".volume-bar");
   const currentTime = document.querySelector(".time-elapsed");
