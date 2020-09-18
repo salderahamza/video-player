83d8b0af645d8655871ccb5136a1af0bc3c47e94
dev-zarl <hamzasafdark@gmail.com>
2020-09-18 22:48:13 +0500

    video progress displayed

--- script.js
+++ script.js
@@ -29,8 +29,18 @@ function togglePlay() {
 video.addEventListener("ended", showPlayIcon);
 // Progress Bar ---------------------------------- //
 
+// Calculate time update format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
 function updateProgress() {
   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
 }
 // Volume Controls --------------------------- //
 
